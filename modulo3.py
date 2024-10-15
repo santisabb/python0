37e8dc8adaeda5edca34ce8380f03ecb52169ee2
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-10-14 23:02:59 -0300

    completa ejercicios propuestos en coursera para proyecto_linkUp

---
+++ modulo3.py
@@ -0,0 +1,18 @@
+# x = float(input("Ingresa un dividendo:"))
+# y = float(input("Ingresa un divisor"))
+# if y!=0:
+#   print("La división es",x/y)
+# print("Fin del programa")
+
+#else print('hoooalaaa') NO PUEDE HABER ELSE SIN IF
+cargo = 'Ejecutivo'
+if cargo == 'Ejecutivo':
+    dinero = 90
+elif cargo == 'Jefe':
+        dinero = 100
+elif cargo == 'Externo':
+        dinero = 50
+
+nro1 = 4
+nro2 = 3
+print (nro1 / nro2)
--- proyecto_linkUp.py
+++ proyecto_linkUp.py
@@ -13,15 +13,53 @@ o888ooooood8 o888o o888o o888o o888o o888o    `YbodP'     888bod8P'
                                                          o888o                                                                     
 """)
 #EJERCICIO 1: solicitar datos al usuario, como edad, país, direccion, nombre completo, etc.
-user_name = input("Ingrese su nombre: ")
-anio_actual = 2024
-user_birth_year = input("Ingrese su año de nacimiento: ")
-int_year = int(user_birth_year)
-user_age = anio_actual - int_year
-user_city = input("Por ultimo ingrese en que ciudad reside actualmente:")
-os.system('cls' if os.name == 'nt' else 'clear')
-print("Perfecto! ya ingresaste todos tus datos, aquí te va una vista de tu perfil")
-print('--------------------------------------------------------------------------------')
-print('Nombre: ' + user_name)
-print('Edad: ' + str(user_age))
-print('Ciudad de residencia: ' + user_city)
+# user_name = input("Ingrese su nombre: ")
+# anio_actual = 2024
+# user_birth_year = input("Ingrese su año de nacimiento: ")
+# int_year = int(user_birth_year)
+# user_age = anio_actual - int_year
+# user_city = input("Por ultimo ingrese en que ciudad reside actualmente:")
+# os.system('cls' if os.name == 'nt' else 'clear')
+# print("Perfecto! ya ingresaste todos tus datos, aquí te va una vista de tu perfil")
+# print('--------------------------------------------------------------------------------')
+# print('Nombre: ' + user_name)
+# print('Edad: ' + str(user_age))
+# print('Ciudad de residencia: ' + user_city)
+
+#EJERCICIO 2: Permitir al usuario decidir si quiere escribir un mensaje
+# d = input('Desea escribir un mensaje para su perfil publico?(S/N)')
+# while d != 'N' and d != 'n':
+#     user_message = input('Ingrese su mensaje:')
+#     d = input('Desea escribir otro mensaje?(S/N)')
+
+#EJERCICIO 3: Desarrollar un menu de opciones que le permita al usuario
+#elegir una opcion y/o salir del programa
+opc = -1
+while opc != 0:
+    print('--------------------------------------------')
+    print('MENU DE OPCIONES')
+    print('--------------------------------------------')
+    print('1- Ingresar datos de usuario')
+    print('2- Publicar un mensaje')
+    print('3- Mostrar datos del usuario')
+    print('0- Salir')
+    opc = int(input('Ingrese una opcion(0 significa salir)'))
+    if opc == 1:
+        user_name = input("Ingrese su nombre: ")
+        anio_actual = 2024
+        user_birth_year = input("Ingrese su año de nacimiento: ")
+        int_year = int(user_birth_year)
+        user_age = anio_actual - int_year
+        user_city = input("Por ultimo ingrese en que ciudad reside actualmente:")
+        os.system('cls' if os.name == 'nt' else 'clear')
+    if opc == 2:
+        user_message = input('Ingrese su mensaje:')
+        os.system('cls' if os.name == 'nt' else 'clear')
+    elif opc == 3:
+        print('Nombre: ' + user_name)
+        print('Edad: ' + str(user_age))
+        print('Ciudad de residencia: ' + user_city)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    elif opc == 0:
+        print('Nos vemos la proxima rey')
+
