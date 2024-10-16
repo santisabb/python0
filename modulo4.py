b715cd1636fa410f192ecd58c258ef1dccc61566
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-10-16 10:32:19 -0300

    agrega nueva funcion que calcula el maximo divisor de un numero en modulo4.py

--- modulo4.py
+++ modulo4.py
@@ -5,5 +5,15 @@ def sayHi2():
     print('Otro saludooo buen diaaaa :D')
     #nota que aquí no uso return
 
+def maxDivisor(n):
+    max_divisor = 0
+    i = 1
+    while i < n:
+        if n % i == 0:
+            max_divisor = i
+        i = i + 1
+    return print(max_divisor)
+
 sayHi()
 sayHi2()
+maxDivisor(10)
