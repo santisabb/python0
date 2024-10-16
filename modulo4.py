5ceb926dfa8fbe4f58a3bbfafb0b1d50743b6850
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-10-16 13:46:11 -0300

    agrega varias funciones en el modulo4.py

--- modulo4.py
+++ modulo4.py
@@ -1,3 +1,7 @@
+from  math import  factorial
+
+print(factorial(5))
+
 def sayHi():
     return print('Hola que tal :)')
 
@@ -14,6 +18,32 @@ def maxDivisor(n):
         i += 1
     return print(max_divisor)
 
+def operation1(n):
+  if n > 10:
+    return 20
+    return 15
+  return 10
+  return 25
+
+def function(x):
+    n = 3
+    return not bool(x % 3)
+
+def funcion(n):
+  a = n ** 3
+  b = a ** 2
+  c = b + 100
+  d = 5 * c
+  return print(d)
+
+d = funcion(2)
+
+print(function(10))
+resultado1 = operation1(8)
+resultado2 = operation1(12)
+
+print(resultado1)
+print(resultado2)
 sayHi()
 sayHi2()
 maxDivisor(10)
